3289d9e412eb201ea75731d0471d76549588ff4e
Sven Sutter <sven.sutter@sotax.com>
2022-05-12 10:55:22 +0200

    Initial commit

---
+++ LS22-Launcher/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace LS22_Launcher
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ LS22-Launcher/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace LS22_Launcher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
